ff227c208470a871f23dfc1d6f0eb541851e5c0a
Pablo <cruzpablomartin@gmail.com>
2021-01-08 23:58:22 -0300

    stringEndsWith

---
+++ exercises/stringEndsWith.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*
+
+Crear una función quede vuelva verdadero si el primer argumento termina con el segundo argumento. 
+OBS: Ambos argumentos deben ser valores de tipo string.
+
+.Ejemplos:
+
+   > stringEndsWith('abc', 'bc');  // return true
+   > stringEndsWith('abc', 'd');  // return false
+
+*/
+
+// Solución:
+
+const stringEndsWith = (str, ending) => str.endsWith(ending);
